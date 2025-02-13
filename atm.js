9bbc289ff9d827498f45bf1bef3c54fc177f895d
khansufyaan315 <khansufyaan315@gmail.com>
2025-02-14 02:15:04 +0530

    Code Change

--- atm.js
+++ atm.js
@@ -1,6 +1,8 @@
 function Atm(b,w){
-    if(w % 100 == 0){
+    if(w == 100 || w == 200 || w == 500 || w == 2000){
         console.log("The balance after withdrawl is " + (b - w));
+    }else{
+        console.log("Invalid amount");
     }
 }
 Atm(5000,200)
